9b96680c55fa9a86f208bef84197e32a1b3164dc
Administracion Barak <matiasari1994@gmail.com>
2026-09-22 15:16:02 -0300

    Actualizar app.py excluyendo pausados de estadisticas

--- app.py
+++ app.py
@@ -652,6 +652,6 @@ def eliminar_banner(id):
     guardar_datos_banners(banners)
     return redirect(url_for('admin'))
 
-if __main__ == '__main__':
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
